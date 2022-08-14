9e0cf277185ff4c6e67107784343425987188170
Admin <Dexter2366@yandex.ru>
2022-08-14 22:12:53 +0300

    HW_27

---
+++ hw_27/ads/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Ad(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=30)
+    price = models.PositiveIntegerField()
+    description = models.TextField(null=True)
+    address = models.CharField(max_length=50)
+    is_published = models.CharField(max_length=50)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
